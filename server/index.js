2393a4b3a8fff2cec041fc65f60ae21771ad3b70
Chris Alvares <mail@chrisalvares.com>
2023-12-04 14:37:03 -0600

    added dotenv for dev.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const log = require('pino')();
